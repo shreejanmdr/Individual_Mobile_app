a6a8877c036cbb535cafc8fb156502499507bf05
shreejanmdr <shreejan0497@gmail.com>
2023-08-03 09:44:02 +0545

    Individual_App_added

---
+++ android/app/src/main/kotlin/com/example/easymart_shreejan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.easymart_shreejan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/viewmodels/single_product_viewmodel.dart
@@ -0,0 +1,42 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:easymart_shreejan/models/category_model.dart';
+// import 'package:easymart_shreejan/models/product_model.dart';
+// import 'package:easymart_shreejan/models/user_model.dart';
+// import 'package:easymart_shreejan/repositories/auth_repositories.dart';
+// import 'package:easymart_shreejan/services/firebase_service.dart';
+// import 'package:easymart_shreejan/viewmodels/global_ui_viewmodel.dart';
+// import '../repositories/category_repositories.dart';
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../models/product_model.dart';
+import '../repositories/product_repositories.dart';
+
+class SingleProductViewModel with ChangeNotifier {
+  ProductRepository _productRepository = ProductRepository();
+  ProductModel? _product = ProductModel();
+  ProductModel? get product => _product;
+
+  Future<void> getProducts(String productId) async{
+    _product=ProductModel();
+    notifyListeners();
+    try{
+      var response = await _productRepository.getOneProduct(productId);
+      _product = response.data();
+      notifyListeners();
+    }catch(e){
+      _product = null;
+      notifyListeners();
+    }
+  }
+
+  Future<void> addProduct(ProductModel product) async{
+    try{
+      var response = await _productRepository.addProducts(product: product);
+    }catch(e){
+      notifyListeners();
+    }
+  }
+
+}
